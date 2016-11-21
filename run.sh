24e1d4d35998d8a8a710cc02d7c971891cd85460
mcparlandj <john.mcparland@cgi.com>
2016-11-21 11:19:06 +0000

    Using jar to run, to allow it to be deployed outside of a container (or rather using an embedded one)

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-mvn -DLOGDIR=target/logs spring-boot:run
+# Old way
+#mvn -DLOGDIR=target/logs spring-boot:run
+
+if [[ 1 -eq ${#} ]];then
+    PORT=${1}
+    echo "Will run on port ${PORT}  
+fi
+
+java -jar knox-solr-ui.jar
