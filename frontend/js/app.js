6434cfbb8ed1c24ad692e3b58674b9e5cc250fd3
Charles Chademana <charles.chademana@cgi.com>
2016-11-22 09:56:47 +0000

    First commit

---
+++ frontend/js/app.js
@@ -0,0 +1,10 @@
+(function() {
+  "use strict";
+
+  var knoxSolrSearch = angular.module('knox_solr_search', []);
+
+  knoxSolrSearch
+    .controller('SearchController',[function () {
+    }]);
+
+})();
