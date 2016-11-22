23d026315b8a97843373beeb810cb8ba992a3785
Charles Chademana <charles.chademana@cgi.com>
2016-11-22 14:45:40 +0000

    Front-end done

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,33 @@
 (function() {
   "use strict";
 
-  var knoxSolrSearch = angular.module('knox_solr_search', []);
+  var knoxSolrSearch = angular.module('knoxSolrSearch', [
+    'ngResource'
+  ]);
 
   knoxSolrSearch
-    .controller('SearchController',[function () {
+    .controller('SearchController', ['$http', function ($http) {
+        var vm = this;
+        vm.q = "";
+
+      // var url = "http://hdp24sandbox.ukwest.cloudapp.azure.com:8983/solr/KnoxIntegrationConfig_shard1_replica1/select?q=*%3A*&wt=json&indent=true";
+      var url = "js/data.json";
+
+      $http.get(url)
+      .then(function (response) {
+        vm.products = response.data.response.docs;
+      }, function (response) {
+        console.log('Error: ' + response.error);
+      });
+
+      vm.searchSolr = function() {
+        vm.q = vm.query;
+      };
+
+      vm.refresh = function() {
+        $route.reload();
+      };
+
     }]);
 
 })();
